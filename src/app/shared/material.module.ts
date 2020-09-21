214560eb36bba23c6c4bf6f9f8b7097363de0701
riskiip <muhamadrizki19@student.undip.ac.id>
2020-09-21 17:16:25 +0700

    Finisihing UI Registration

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const MaterialComponent = [
   MatCardModule,
-  MatButtonModule
+  MatButtonModule,
+  MatDialogModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule
 ];
 
 @NgModule({
