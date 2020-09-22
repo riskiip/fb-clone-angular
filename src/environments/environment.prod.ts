98e79ace0ccc3f31b383f8c5a182fec9ad916569
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.iid>
2020-09-22 17:35:01 +0700

    support: add firebaseConfig into environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCjdpGAZ4nzCyiG60s13dvh3vxw1GhSUjg",
+    authDomain: "cloningfb-2821e.firebaseapp.com",
+    databaseURL: "https://cloningfb-2821e.firebaseio.com",
+    projectId: "cloningfb-2821e",
+    storageBucket: "cloningfb-2821e.appspot.com",
+    messagingSenderId: "968939482504",
+    appId: "1:968939482504:web:c5d556d82a0b4dff24de3d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCjdpGAZ4nzCyiG60s13dvh3vxw1GhSUjg",
+    authDomain: "cloningfb-2821e.firebaseapp.com",
+    databaseURL: "https://cloningfb-2821e.firebaseio.com",
+    projectId: "cloningfb-2821e",
+    storageBucket: "cloningfb-2821e.appspot.com",
+    messagingSenderId: "968939482504",
+    appId: "1:968939482504:web:c5d556d82a0b4dff24de3d"
+  }
 };
 
 /*
